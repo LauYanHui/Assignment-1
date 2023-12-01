b622a70790418dad65d243eaac3eebde5e9be958
LauYanHui <S10241639@connect.np.edu.sg>
2023-12-01 23:28:04 +0800

    changed abit

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";  
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} // Add this line
-    setTimeout(showSlides, 10000); // Change image every 2 seconds
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
   
